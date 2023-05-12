90d469d539fe34b07f6b42f1daec15d91ebff8c9
Murali <murali@Murali-K-Tech.local>
2023-05-13 02:01:53 +0530

    errors handled

--- generate-app.js
+++ generate-app.js
@@ -3,16 +3,13 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
-const chalk = import('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const inquirer = import('./questions');
-const oraPromise = import('ora');
 
 if (process.argv.length < 3) {
     console.log('You have to provide a name to your app.');
-    console.log('For example :');
-    console.log('    npx create-my-boilerplate my-app');
+    console.log('For example:');
+    console.log('    npx my-cli <project>');
     process.exit(1);
 }
 
@@ -33,43 +30,53 @@ try {
     process.exit(1);
 }
 
+// Cleanup function to delete the created folder
+async function cleanup() {
+    const chalk = await import('chalk');
+    console.log(chalk.default.yellow('\nCleaning up...'));
+    try {
+        fs.rmSync(projectPath, { recursive: true, force: true });
+        console.log(chalk.default.red('installation cancelled:'));
+        console.log(chalk.default.green('Cleanup completed.'));
+    } catch (err) {
+        console.log(chalk.default.red('Error during cleanup:', err));
+    }
+    process.exit(1);
+}
+
+
 async function main() {
     try {
         clear();
-        console.log(chalk.blue(figlet.textSync('Careers360 CLI', { horizontalLayout: 'full' })));
-        const setupParameters = await inquirer.setupQuestions();
-        console.log(chalk.greenBright("\n\nInitializing Project...!!!!\n\n"));
+        const chalk = await import('chalk');
+        const ora = await import('ora');
+        const inquirer = await import('inquirer');
+        console.log(chalk.default.blue(figlet.textSync('Careers360 CLI', { horizontalLayout: 'full' })));
+        // const setupParameters = await inquirer?.prompt([
+        //     // your setup questions here
+        // ]);
+        console.log(chalk.default.blue("\n\nInitializing Project...!!!!\n\n"));
 
-        const spinner = ora('Installing dependencies...').start();
-        execSync('npm config set @cnext:registry https://npm.careers360.com');
+        const spinner = ora.default('Installing dependencies...').start();
+        spinner.start()
+        // execSync('npm config set @cnext:registry https://npm.careers360.com');
         execSync('npm i --legacy-peer-deps');
         spinner.succeed('Dependencies installed successfully!');
 
         // ...
 
-        console.log(
-            chalk.greenBright(
-                "\nThe installation is done, this is ready to use !\n"
-            )
-        );
-        console.log(
-            chalk.greenBright(
-                `\ncd ${projectName}`
-            )
-        );
-        console.log(
-            chalk.greenBright(
-                `npm run dev`
-            )
-        );
-        console.log(
-            chalk.greenBright(
-                `\nEnjoy Coding...! \n`
-            )
-        );
+        console.log(chalk.default.green("\nThe installation is done. This is ready to use!\n"));
+        console.log(chalk.default.green(`\ncd ${projectName}`));
+        console.log(chalk.default.green("npm run dev"));
+        console.log(chalk.default.green("\nEnjoy Coding...! \n"));
 
     } catch (error) {
-        console.log(error);
+        try {
+            // Handle the SIGINT signal (manually interrupting the process)
+            process.on('SIGINT', cleanup);
+        } catch {
+            console.log(error)
+        }
     }
 }
 
